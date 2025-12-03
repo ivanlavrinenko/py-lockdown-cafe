39a9d432ee55334249348f578692584af9e921d8
Ivan Lavrinenko <lavrinenkoivan51@gmail.com>
2025-12-03 04:33:19 +0100

    Cafe class and corresponding errors for validation in functions are implemented

---
+++ app/cafe.py
@@ -0,0 +1,19 @@
+from datetime import date
+
+from app.errors import (NotVaccinatedError,
+                        OutdatedVaccineError,
+                        NotWearingMaskError)
+
+
+class Cafe:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def visit_cafe(self, visitor: dict) -> str:
+        if not visitor.get("vaccine"):
+            raise NotVaccinatedError("Visitor is not vaccinated")
+        if visitor["vaccine"]["expiration_date"] < date.today():
+            raise OutdatedVaccineError("Visitor has outdated vaccine")
+        if visitor["wearing_a_mask"] is False:
+            raise NotWearingMaskError("Visitor nis not wearing mask")
+        return f"Welcome to {self.name}"
